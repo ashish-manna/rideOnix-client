449366120a146420f08504fd934a8dbc5d50f3ab
ashish-manna <m.ashish1920@gmail.com>
2025-09-06 22:05:28 +0530

    basic home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="bg-[url(/images/hero-banner.jpg)] bg-top-right bg-cover w-full h-screen flex flex-col justify-between">
+            <h1 className="text-2xl font-bold text-white px-4 py-6">RideOnix</h1>
+            <div className="bg-white w-full flex flex-col gap-4 px-4 py-6 rounded-t-xl">
+                <h2 className="text-2xl text-black font-bold">Get started with RideOnix</h2>
+                <div className="bg-black px-2 py-4 text-white flex justify-center items-center rounded-xl">
+                    <Link className="text-xl font-semibold" to={'/user-login'}> Continue </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
