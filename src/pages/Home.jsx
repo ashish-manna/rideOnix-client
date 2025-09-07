ad6afa0c461a8773aff6ce62cd239662557e3343
ashish-manna <m.ashish1920@gmail.com>
2025-09-07 18:27:52 +0530

    user login page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <div className="bg-white w-full flex flex-col gap-4 px-4 py-6 rounded-t-xl">
                 <h2 className="text-2xl text-black font-bold">Get started with RideOnix</h2>
                 <div className="bg-black px-2 py-4 text-white flex justify-center items-center rounded-xl">
-                    <Link className="text-xl font-semibold" to={'/user-login'}> Continue </Link>
+                    <Link className="text-xl font-semibold" to={'/login'}> Continue </Link>
                 </div>
             </div>
         </div>
