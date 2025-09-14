b6ef5daeed08e6f1c5f9bc6e90f75971b2e7f5c7
ashish-manna <m.ashish1920@gmail.com>
2025-09-14 23:43:24 +0530

    captain register page updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Home } from "./pages/Home"
 import Layout from "./components/Layout"
 import UserContext from "./context/UserContext"
 import { useState } from "react"
+import Start from "./pages/Start"
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -13,11 +14,12 @@ const App = () => {
     <>
       <UserContext.Provider value={{ user, setUser }}>
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="home" element={<Home />} />
+          <Route path="/" element={<Start />} />
+          <Route element={<Layout />}>
+            <Route path="/home" element={<Home />} />
           </Route>
-          <Route path="login" element={<UserLogin />} />
-          <Route path="register" element={<CaptainLogin />} />
+          <Route path="/login" element={<UserLogin />} />
+          <Route path="/register" element={<CaptainLogin />} />
         </Routes>
       </UserContext.Provider>
     </>
--- src/pages/CaptainLogin.jsx
+++ src/pages/CaptainLogin.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const CaptainLogin = () => {
-
     const [isLogin, setIsLogin] = useState(true);
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+    const [vehicleColor, setVehicleColor] = useState("");
+    const [vehicleCapacity, setVehicleCapacity] = useState("");
+    const [vehiclePlate, setVehiclePlate] = useState("");
+    const [vehicleType, setVehicleType] = useState("car");
 
     const loginHandler = () => {
         console.log(email, password);
@@ -16,27 +19,73 @@ const CaptainLogin = () => {
     }
     return (
         <div className="w-full min-h-screen flex flex-col">
-            <div>
-                <h1 className="text-3xl font-bold text-black px-4 py-6">RideOnix</h1>
-            </div>
-            <div className="w-full px-4 flex flex-col gap-2 pt-5">
-                {!isLogin && <div className="w-full flex flex-col gap-1">
-                    <div className="text-lg font-semibold ">Whats Your Name</div>
-                    <div className="w-full flex gap-2">
-                        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="bg-gray-200 text-black w-full p-2 rounded-md" placeholder="First name" />
-                        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} className="bg-gray-200 text-black w-full p-2 rounded-md" placeholder="Last name" />
+            <div className="w-full px-4 flex flex-col gap-1 pt-5">
+                {!isLogin && (
+                    <div className="w-full flex flex-col gap-1">
+                        <div className="text-lg font-semibold">What's Your Name</div>
+                        <div className="w-full flex gap-2">
+                            <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="bg-gray-200 text-black w-full p-2 rounded-md" placeholder="First name" />
+                            <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} className="bg-gray-200 text-black w-full p-2 rounded-md" placeholder="Last name" />
+                        </div>
                     </div>
-                </div>
-                }
-
+                )}
                 <div className="w-full flex flex-col gap-1">
-                    <div className="text-lg font-semibold ">Whats's your email</div>
+                    <div className="text-lg font-semibold">What's your email</div>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-200 text-black w-full p-2 rounded-md" placeholder="example@gmail.com" />
                 </div>
                 <div className="w-full flex flex-col gap-1">
                     <div className="text-lg font-semibold">Enter password</div>
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-200 text-black w-full p-2 rounded-md" placeholder="password" />
                 </div>
+                {!isLogin && (
+                    <>
+                        <div className="w-full flex flex-col gap-2 mt-4">
+                            <div className="text-lg font-semibold">Vehicle Information</div>
+                            <div className="flex w-full justify-between gap-1">
+                                <div className="w-full flex flex-col gap-1">
+                                    <input
+                                        type="text"
+                                        value={vehicleColor}
+                                        onChange={(e) => setVehicleColor(e.target.value)}
+                                        className="bg-gray-200 text-black w-full p-2 rounded-md"
+                                        placeholder="Vehicle Color"
+                                    />
+                                </div>
+                                <div className="w-full flex flex-col gap-1">
+                                    <input
+                                        type="number"
+                                        value={vehicleCapacity}
+                                        onChange={(e) => setVehicleCapacity(e.target.value)}
+                                        className="bg-gray-200 text-black w-full p-2 rounded-md"
+                                        placeholder="Vehicle Capacity"
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex w-full justify-between gap-1">
+                                <div className="w-full flex flex-col gap-1">
+                                    <input
+                                        type="number"
+                                        value={vehiclePlate}
+                                        onChange={(e) => setVehiclePlate(e.target.value)}
+                                        className="bg-gray-200 text-black w-full p-2 rounded-md"
+                                        placeholder="Vehicle Plate Number"
+                                    />
+                                </div>
+                                <div className="w-full flex flex-col gap-1">
+                                    <select
+                                        value={vehicleType}
+                                        onChange={(e) => setVehicleType(e.target.value)}
+                                        className="bg-gray-200 text-black w-full h-full rounded-md"
+                                    >
+                                        <option value="car">Car</option>
+                                        <option value="auto">Auto</option>
+                                        <option value="moto">Moto</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )}
             </div>
             <div className="w-full p-4 pb-6 flex flex-col grow justify-between">
                 <div className="pt-5 text-center">
